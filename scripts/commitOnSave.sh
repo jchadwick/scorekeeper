fbc9cc62514ab7f10e2c7e9616683e065db08f4e
Jess Chadwick <jesschadwick@gmail.com>
2023-04-28 16:25:30 -0400

    WIP

--- scripts/commitOnSave.sh
+++ scripts/commitOnSave.sh
@@ -2,13 +2,8 @@
 
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [ "$branch" != "wip" ]
+if [ "$branch" == "wip" ]
 then
     git add .
-    git stash
-    git checkout wip
-    git stash pop
+    git commit -am "WIP"
 fi
-
-git add .
-git commit -am "WIP"
