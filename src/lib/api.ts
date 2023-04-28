1bb157300cd9b6d1dadda98508f5a170a6da1aab
Jess Chadwick <jchadwick@glgroup.com>
2023-04-28 19:42:59 -0400

    WIP

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,22 @@
 import axios from "axios"
 
+export interface Player {
+    id: string;
+    name: string;
+}
+
+export interface Game {
+    id: string;
+    name: string;
+}
+
+export interface GamePlayer {
+    gameId: string;
+    playerId: string;
+    score: number;
+    level?: number;
+}
+
 export const api = axios.create({ baseURL: `${process.env.API_URL}/api` })
 
 export default api
