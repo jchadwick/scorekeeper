13a3b24a2c5d52a5954f796206050b855979ea90
Jess Chadwick <jesschadwick@gmail.com>
2023-04-29 13:27:10 -0400

    WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
