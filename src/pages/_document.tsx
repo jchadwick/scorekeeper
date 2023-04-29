133bbe9efce8357e373d38717b2493a0437d32af
Jess Chadwick <jchadwick@glgroup.com>
2023-04-29 09:16:51 -0400

    WIP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={`flex min-h-screen flex-col items-center p-24`}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
