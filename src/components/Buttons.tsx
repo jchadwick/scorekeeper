ec51cac611feec61c0681a2cdec908d913364dea
Jess Chadwick <jesschadwick@gmail.com>
2023-04-29 19:24:23 -0400

    WIP

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,25 @@
+import {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  PropsWithChildren,
+} from "react";
+
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & { variant?: "primary" | "secondary" | "warning" | "error" };
+
+export const Button = ({ variant, ...props }: ButtonProps) => {
+  const color = 
+  variant === "secondary" ? "gray" :
+  variant === "warning" ? "orange" : 
+  variant === "error" ? "red" :
+  : "blue";
+
+  return (
+    <button
+      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+      {...props}
+    />
+  );
+};
