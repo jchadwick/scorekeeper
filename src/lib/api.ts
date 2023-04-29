0896c292b2b3cf8c2a6b0a55691a58afe6fb2495
Jess Chadwick <jchadwick@glgroup.com>
2023-04-29 09:07:18 -0400

    WIP

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export type * from "./api.model"
 
-export const api = axios.create({ baseURL: `${process.env.API_URL}/api` })
+export const api = axios.create({ baseURL: `${process.env.API_URL || ""}/api` })
 
 export default api
